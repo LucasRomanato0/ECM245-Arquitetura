c59884b80b2787d9bcf8131d41d10539864f30bf
LucasRomanato0 <20.00313-7@maua.br>
2022-06-02 19:10:26 -0300

    Assemblador

---
+++ Assemblador/Assemblador.py
@@ -0,0 +1,28 @@
+fileName = input('Nome do arquivo de leitura: ')
+file1 = open(fileName,'r')
+Lines = file1.readlines()
+
+fileNameResult = input('Nome do arquivo para armezenar os resultados: ')
+result = open(fileNameResult, 'w')
+
+conversion = {"HLT":" : 00",
+              "STO":" : 1",
+              "LD":" : 2",
+              "LDI":" : 3",
+              "ADD":" : 4",
+              "ADDI":" : 5",
+              "SUB":" : 6",
+              "SUBI":" : 7"}
+
+count = 0
+n = 0
+# Strips the newline character
+for line in Lines:
+    try:
+        split = line.strip().split(" ")
+        result.write(str(hex(n)).upper()[2:] + conversion[split[0]] + split[1] + "\n")
+        print(line.strip())
+        n = n+1
+    except:
+        pass
+result.close()
